bcd355090f0119e924657540175ef2b0e460096f
Lillian Yang <lyang@lcs.on.ca>
2021-12-11 10:41:58 -0500

    Got may static view of WaterIntake.

--- WaterIntake/ContentView.swift
+++ WaterIntake/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(Color.blue)
+                                
+                VStack{
+                    Image("water")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                    
+                    Text("How much water did you intake today?")
+                        .bold()
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    
+                    TextField("This argument should the describe the purpose of the text field within the context of your app, e.g.: 'Name' or 'Email Address'",
+                              text: .constant(""),
+                              prompt: Text("Enter your water intake here..."))
+                        .padding()
+                        .font(.title3)
+                    
+                    Button(action: {
+                        // NOTE: Output will not be shown unless this app is run in the "full" simulator
+                        print("Button was pressed")
+                    }, label: {
+                        Text("Submit")
+                    })
+                    .foregroundColor(.black)
+                    .buttonStyle(.bordered)
+                    .background(.gray)
+                    .cornerRadius(15)
+                    .padding()
+                    
+                    Text("Feedback:")
+                        .bold()
+                        .font(.title)
+                    Text("You need to drink more!")
+                        .padding()
+                        .font(.title3)
+                    
+                    
+                }
+            }
+            
+        }
+        .navigationTitle("Water Intake")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
+       
     }
 }
