76c3751c49790c21420c718167388afe9f23a0b8
Lillian Yang <lyang@lcs.on.ca>
2021-12-11 10:06:16 -0500

    Initial Commit

---
+++ WaterIntake/WaterIntakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterIntakeApp.swift
+//  WaterIntake
+//
+//  Created by Lillian Yang on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct WaterIntakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
